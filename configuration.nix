60adb7b6041fac9bd3dd47beb73a39c8674c3c86
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-06-07 19:36:23 -0400

    Fix a punctuation error in a comment in the configuration.nix.

--- configuration.nix
+++ configuration.nix
@@ -449,7 +449,7 @@
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # (e.g., run man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
 }
