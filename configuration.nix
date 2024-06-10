8cd2d47e4b49dbe0341c9523b4a514d3ef0f6849
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-06-10 08:28:07 -0400

    Delete a not-needed extra comment about stateVersion in the configuration.nix file.

--- configuration.nix
+++ configuration.nix
@@ -451,6 +451,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g., run man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11";
 
 }
